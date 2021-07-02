22f870207813a5c369e32827302abfeb7f83208d
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2021-07-02 13:57:29 +0530

    add benchmark

--- README.md
+++ README.md
@@ -11,3 +11,20 @@ In another terminal
 ```
 docker-compose exec benchmark bash /srv/test/benchmark.sh
 ```
+
+On my system this returns
+
+```
+dcmsend on pynetdicom:5252
+
+real    0m6.724s
+user    0m0.697s
+sys     0m0.080s
+--
+dcmsend on dcmtk:5252
+
+real    0m51.090s
+user    0m0.743s
+sys     0m0.186s
+--
+```
--- benchmark.sh
+++ benchmark.sh
@@ -4,10 +4,12 @@ cd "$(dirname "$0")"
 
 tar -xzf large_dicom_dir.tar.gz
 
-port=5252
-hosts=(pynetdicom dcmtk)
-for host in "${hosts[@]}"
+servers=(pynetdicom:5252 dcmtk:5252)
+for server in "${servers[@]}"
 do
+    arr=(${server//:/ })
+    host=${arr[0]}
+    port=${arr[1]}
     echo "dcmsend on $host:$port"
     echoscu $host $port
     time dcmsend $host $port large_dicom_dir/*
